980d919e822a5b4fc6639decab941361bc669fbf
madebypomelo <andreasksk@gmail.com>
2020-05-27 08:49:01 +0200

    Disconnected update example

--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -17,9 +17,33 @@ namespace ConsoleApp
 			//AddSamurai();
 			//GetSamurais("After Add:");
 			//InsertMultipleSamurais();
-			QueryFilters("Sara");
+			//QueryFilters("Sara");
+			QueryAndUpdateBattle_Disconnected();
 			Console.Write("Press any key...");
 			Console.ReadKey();
+		}
+
+		private static void InsertBattle()
+		{
+			_context.Battles.Add(new Battle()
+			{
+				Name = "Battle of Okehazama",
+				StartDate = new DateTime(1560, 05, 01),
+				EndDate = new DateTime(1560, 06, 15)
+			});
+			_context.SaveChanges();
+		}
+
+		private static void QueryAndUpdateBattle_Disconnected()
+		{
+			var battle = _context.Battles.AsNoTracking().FirstOrDefault();
+			battle.EndDate = new DateTime(1560, 06, 30);
+
+			using (var newContextInstance = new SamuraiContext())
+			{
+				newContextInstance.Battles.Update(battle);
+				newContextInstance.SaveChanges();
+			}
 		}
 
 		private static void InsertMultipleSamurais()
