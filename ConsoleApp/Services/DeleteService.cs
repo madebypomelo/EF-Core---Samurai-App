d006fef45e843d35421589150e279570399f4b68
madebypomelo <andreasksk@gmail.com>
2020-05-27 08:37:53 +0200

    Added delete methods

---
+++ ConsoleApp/Services/DeleteService.cs
@@ -0,0 +1,32 @@
+﻿using SamuraiApp.Data;
+using SamuraiApp.Domain;
+
+namespace ConsoleApp.Services
+{
+    public class DeleteService
+    {
+        private static readonly SamuraiContext _context = new SamuraiContext();
+
+        public static void DeleteProfile(int samuraiID)
+        {
+			#region 1. Best practice/normal procedure.
+			// Fetch samurai and horse.
+			var samurai = _context.Samurais.Find(samuraiID);
+
+            _context.RemoveRange(samurai, samurai.Horse);
+			#endregion
+
+			#region 2. Bad practice/may have side effects.
+			// Construct fake object, only holding the PrimaryKey.
+			samurai = new Samurai()
+			{
+				ID = samuraiID
+			};
+
+			_context.RemoveRange(samurai, samurai.Horse);
+			#endregion
+
+			_context.SaveChanges();
+		}
+	}
+}
