0b50b8610566cd7c03f71c2e7c855c56ec4f9b39
madebypomelo <andreasksk@gmail.com>
2020-05-27 08:57:53 +0200

    Added NoTracking context.

---
+++ SamuraiApp.Data/SamuraiContextNoTracking.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
+using SamuraiApp.Domain;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SamuraiApp.Data
+{
+    public class SamuraiContextNoTracking : DbContext
+    {
+		#region Constructors.
+        SamuraiContextNoTracking()
+        {
+            // Avoid queries to be tracked by outr DBContext by default.
+            // Use _context.AsTracking() in those cases, where you need to track a query.
+            ChangeTracker.QueryTrackingBehavior = QueryTrackingBehavior.NoTracking;
+        }
+        #endregion
+
+        public DbSet<Samurai> Samurais { get; set; }
+        public DbSet<Quote> Quotes { get; set; }
+        public DbSet<Clan> Clans { get; set; }
+        public DbSet<Battle> Battles { get; set; }
+
+        public static readonly ILoggerFactory ConsoleLoggerFactory = LoggerFactory.Create(builder =>
+        {
+            builder.AddFilter((category, level) =>
+            category == DbLoggerCategory.Database.Command.Name
+            && level == LogLevel.Information)
+            .AddConsole();
+        });
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder
+                .UseLoggerFactory(ConsoleLoggerFactory)
+                    .UseSqlServer("Data Source = (localdb)\\MSSQLLocalDB; Initial Catalog = SamuraiApp");
+
+
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<SamuraiBattle>().HasKey(s => new { s.SamuraiID, s.BattleID });
+
+            modelBuilder.Entity<Horse>().ToTable("Horses");
+        }
+    }
+}
